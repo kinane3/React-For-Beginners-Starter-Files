5a12cb83f2821c63c8d74ad464a45dd51533fe1d
John Kinane <john@kinane.net>
2019-04-07 22:50:03 -0400

    progress

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
     render() {
         return (
             <header className ="top">
-                <h1>Panel
+                <h1>Catch
                     <span className="ofThe">
                         <span className="of">Of</span>
                         <span className="the">the</span>
