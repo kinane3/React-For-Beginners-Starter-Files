75fd71331fb6c99c6eca46d49c7d438fca8be43f
John Kinane <john@kinane.net>
2019-04-10 16:22:33 -0400

    beginning of Chap 16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,16 +2,46 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './fish';
 
 class App extends React.Component {
+    state = {
+        fishes: {
+
+        },
+        order: {}
+    };
+    addFish = fish => {
+        // 1. Take a copy of the existing state
+        const fishes = {...this.state.fishes};
+        // 2. Add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. Set the new fishes objet to state
+        this.setState({
+            //fishes: fishes //old way of doing it
+            fishes //ES6 way of doing above
+        });
+    }
+
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    }
+
     render () {
         return (
             <div className ="catch-of-the-day">
                 <div className ="menu">
-                    <Header tagline="say wah"/>
+                    <Header tagline="Fish, Baby, Fish!"/>
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+                    </ul>
                 </div>
                  <Order />               
-                <Inventory />
+                <Inventory 
+                    addFish={this.addFish} 
+                    loadSampleFishes={this.loadSampleFishes}
+                />
             </div>
         )
     }
