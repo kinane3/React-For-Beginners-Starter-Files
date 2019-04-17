0ec915a764eb30b2063cc65dc824c5fbf97722c2
John Kinane <john@kinane.net>
2019-04-17 12:47:13 -0400

    end of chap 16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,50 +1,66 @@
-import React from 'react';
-import Header from './Header';
-import Order from './Order';
-import Inventory from './Inventory';
-import sampleFishes from '../sample-fishes';
-import Fish from './fish';
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./fish";
 
 class App extends React.Component {
-    state = {
-        fishes: {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // 1. Take a copy of the existing state / property spread notation https://stackoverflow.com/a/31049016/823520
+    const fishes = { ...this.state.fishes };
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes objet to state
+    this.setState({
+      //fishes: fishes //old way of doing it
+      fishes //ES6 way of doing above
+    });
+  };
 
-        },
-        order: {}
-    };
-    addFish = fish => {
-        // 1. Take a copy of the existing state
-        const fishes = {...this.state.fishes};
-        // 2. Add our new fish to that fishes variable
-        fishes[`fish${Date.now()}`] = fish;
-        // 3. Set the new fishes objet to state
-        this.setState({
-            //fishes: fishes //old way of doing it
-            fishes //ES6 way of doing above
-        });
-    }
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
 
-    loadSampleFishes = () => {
-        this.setState({ fishes: sampleFishes });
-    }
+  addToOrder = key => {
+    // take a copy of state // property spread notation https://stackoverflow.com/a/31049016/823520
+    const order = { ...this.state.order };
 
-    render () {
-        return (
-            <div className ="catch-of-the-day">
-                <div className ="menu">
-                    <Header tagline="Fish, Baby, Fish!"/>
-                    <ul className="fishes">
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
-                    </ul>
-                </div>
-                 <Order />               
-                <Inventory 
-                    addFish={this.addFish} 
-                    loadSampleFishes={this.loadSampleFishes}
-                />
-            </div>
-        )
-    }
+    // either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+
+    // call setState to update our state object
+    this.setState({ order });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fish, Baby, Fish!" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
+        </div>
+        <Order />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- catch-of-the-day/src/components/fish.js
+++ catch-of-the-day/src/components/fish.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { formatPrice } from '../helpers';
+import React from "react";
+import { formatPrice } from "../helpers";
 class Fish extends React.Component {
-    render() {
-        const {image, name, price, desc, availability, status} = this.props.details;
-        const isAvailable = Fish.status === 'available';
-        return (
-            <li  className="menu-fish">
-                <img src={image} alt={name} />
-                <h3 className="fish-name">
-                    {name}
-                    <span className="price">{formatPrice(price)}</span>
-                </h3>
-                <p>{desc}</p>
-                <button disabled={!isAvailable}>Add to Cart</button>
-            </li>
-        )
-    } 
+  render() {
+    const {
+      image,
+      name,
+      price,
+      desc,
+      availability,
+      status
+    } = this.props.details;
+    const isAvailable = status === "available";
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
+          {isAvailable ? "Add To Order" : "Sold Out!"}
+        </button>
+      </li>
+    );
+  }
 }
 
 export default Fish;
