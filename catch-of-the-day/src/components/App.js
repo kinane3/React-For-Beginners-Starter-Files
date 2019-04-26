b2c4501bd9f95e05eba7d801b7f1783585e2789e
John Kinane <john@kinane.net>
2019-04-26 16:11:04 -0400

    format cleaned up

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
     const { params } = this.props.match;
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
-      state: 'fishes'
+      state: "fishes"
     });
   }
 
